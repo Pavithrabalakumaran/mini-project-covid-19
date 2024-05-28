3f1c431a2f46bd6cd8bb272d0664a8160a8c9d08
Pavithrabalakumaran <pavithrachandrasekar9360@gmail.com>
2024-05-28 18:54:43 +0000

    mp3

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,41 +1,102 @@
-import {Link} from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom'
+import {Component} from 'react'
 
 import './index.css'
 
-const Navbar = () => (
-  <nav className="navbar-container">
-    <div className="container">
-      <Link to="/" className="img-link">
-        <img
-          src="https://res.cloudinary.com/dbweo4cmc/image/upload/v1625723673/COVID19INDIA_tvtxet.png"
-          alt="logo"
-          className="covid-logo"
-        />
-      </Link>
+class Navbar extends Component {
+  state = {
+    showMenu: false,
+  }
 
-      <ul className="nav-items">
-        <li>
-          <Link to="/" className="nav-home">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" className="nav-about">
-            About
-          </Link>
-        </li>
-      </ul>
-      <div className="addQ-container">
-        <Link to="/tabs">
-          <img
-            className="addQueue"
-            src="https://res.cloudinary.com/dbweo4cmc/image/upload/v1625949520/add-to-queue_1_n0hju7.png"
-            alt="addQueue"
-          />
-        </Link>
-      </div>
-    </div>
-  </nav>
-)
+  toggleMenu = () =>
+    this.setState(prevState => ({showMenu: !prevState.showMenu}))
 
-export default Navbar
+  closeMenu = () => this.setState({showMenu: false})
+
+  render() {
+    const {showMenu} = this.state
+    const {match} = this.props
+    const {path} = match
+    const homeClassName = path === '/' ? 'link-name highlight' : 'link-name'
+    const aboutClassName =
+      path === '/about' ? 'link-name highlight' : 'link-name'
+
+    return (
+      <>
+        <nav className="navbar-container">
+          <div className="container">
+            <Link to="/" className="img-link">
+              <img
+                src="https://res.cloudinary.com/dbweo4cmc/image/upload/v1625723673/COVID19INDIA_tvtxet.png"
+                alt="logo"
+                className="covid-logo"
+              />
+            </Link>
+
+            <ul className="nav-items">
+              <li key="1">
+                <Link to="/" className="nav-home">
+                  <button type="button" className={homeClassName}>
+                    Home
+                  </button>
+                </Link>
+              </li>
+
+              <li key="2">
+                <Link to="/about" className="nav-about">
+                  <button type="button" className={aboutClassName}>
+                    About
+                  </button>
+                </Link>
+              </li>
+            </ul>
+
+            <button
+              type="button"
+              className="menu-button"
+              onClick={this.toggleMenu}
+            >
+              <img
+                className="addQueue"
+                src="https://res.cloudinary.com/dbweo4cmc/image/upload/v1625949520/add-to-queue_1_n0hju7.png"
+                alt="addQueue"
+              />
+            </button>
+          </div>
+        </nav>
+
+        {showMenu ? (
+          <ul className="nav-items">
+            <li key="1">
+              <Link to="/" className="nav-home">
+                <button type="button" className={homeClassName}>
+                  Home
+                </button>
+              </Link>
+            </li>
+
+            <li key="2">
+              <Link to="/about" className="nav-about">
+                <button type="button" className={aboutClassName}>
+                  About
+                </button>
+              </Link>
+            </li>
+
+            <li className="close-item" key="2">
+              <button
+                type="button"
+                className="close-button"
+                onClick={this.closeMenu}
+              >
+                close
+              </button>
+            </li>
+          </ul>
+        ) : null}
+      </>
+    )
+  }
+}
+
+export default withRouter(Navbar)
